1a18111db9aefdcc39b41a1e9b974b5324b7cb57
Test12378 <Test12378@users.noreply.github.com>
2026-08-26 21:58:05 +0000

    Auto-deploy 48bfc720b521430b8645d6346898183be835a2e0 —

--- assets/useBackgroundRefreshListener-DbkIJvNL.js
+++
@@ -1,2 +0,0 @@
-import{r as e}from"./rolldown-runtime-S-ySWqyJ.js";import{_ as t}from"./charts-BAw3Nsqw.js";import{t as n}from"./index-B7vI_w-i.js";var r=e(t(),1);function i(e){(0,r.useEffect)(()=>{let t=()=>{e()};return window.addEventListener(n,t),()=>window.removeEventListener(n,t)},[e])}export{i as t};
-//# sourceMappingURL=useBackgroundRefreshListener-DbkIJvNL.js.map
