91a96914e6bc1f93d5c4959a2d64ba4a19a36499
Test12378 <Test12378@users.noreply.github.com>
2026-08-26 21:24:50 +0000

    Auto-deploy 2d806d10b21da9bb36141a563d29126fbd5f3ae3 —

---
+++ assets/useBackgroundRefreshListener-Djf57RFD.js
@@ -0,0 +1,2 @@
+import{r as e}from"./rolldown-runtime-S-ySWqyJ.js";import{_ as t}from"./charts-BAw3Nsqw.js";import{t as n}from"./index-ChtcyLAW.js";var r=e(t(),1);function i(e){(0,r.useEffect)(()=>{let t=()=>{e()};return window.addEventListener(n,t),()=>window.removeEventListener(n,t)},[e])}export{i as t};
+//# sourceMappingURL=useBackgroundRefreshListener-Djf57RFD.js.map
